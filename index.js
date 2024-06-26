ded6c444fa13bdc568c6dcb84b37359b3da1a76a
ifshad <shoronshark@gmail.com>
2024-06-26 22:25:27 +0600

    routes created

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log('created teh server')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lbhoupe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const productsCollection = db.collection("tech-products");
+    const cursor = productsCollection.find();
+    const result = await cursor.toArray();
+    res.json(result);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("TechDynasty store server running properly...");
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
+app.listen(port, () => {
+  console.log(`Running on port:${port}`);
+});
+
+module.exports = app;
