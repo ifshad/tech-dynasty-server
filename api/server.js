cc34c5bd5e81de19e70ce1d261147ed68cd555aa
ifshad <shoronshark@gmail.com>
2024-06-26 22:41:48 +0600

    routes created and vercel setup created

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "..", ".env") });
+
+const app = require("../index.js");
+
+module.exports = (req, res) => {
+  app(req, res);
+};
--- index.js
+++ index.js
@@ -33,6 +33,102 @@ app.get("/products", async (req, res) => {
   }
 });
 
+app.get("/users", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const usersCollection = db.collection("tech-users");
+    const cursor = usersCollection.find();
+    const result = await cursor.toArray();
+    res.json(result);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get("/products/:id", async (req, res, next) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const productsCollection = db.collection("tech-products");
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await productsCollection.findOne(query);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/products", async (req, res, next) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const productsCollection = db.collection("tech-products");
+    const product = req.body;
+    const result = await productsCollection.insertOne(product);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/users", async (req, res, next) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const usersCollection = db.collection("tech-users");
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/products/:id", async (req, res, next) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const productsCollection = db.collection("tech-products");
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await productsCollection.deleteOne(query);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put("/products/:id", async (req, res, next) => {
+  try {
+    await client.connect();
+    const db = client.db("tech-dynasty");
+    const productsCollection = db.collection("tech-products");
+    const updatedDoc = req.body;
+    const product = {
+      $set: {
+        name: updatedDoc.updatedName,
+        imageUrl: updatedDoc.updatedImageUrl,
+        brandName: updatedDoc.updatedBrandName,
+        price: updatedDoc.updatedPrice,
+        description: updatedDoc.updatedDescription,
+        rating: updatedDoc.updatedRating,
+      },
+    };
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+    const result = await productsCollection.updateOne(query, product, options);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("TechDynasty store server running properly...");
 });
